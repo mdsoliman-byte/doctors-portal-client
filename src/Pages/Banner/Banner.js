56e0e2b0056d843cb2b68e6f88ca5af072de649f
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-05-18 03:24:59 +0600

    home page benner section complite

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import cahir from "../../assets/images/chair.png"
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen px-12 ">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={cahir} class="max-w-sm rounded-lg shadow-2xl" />
+                <div className=''>
+                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-primary to-secondary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
