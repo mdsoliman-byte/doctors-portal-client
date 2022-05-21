c3122608c9154cd34b099345e93b9cca112c0b81
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-05-21 16:28:53 +0600

    appoientment card desigine complite

--- src/Pages/Appointment/AbailabailAppointment.js
+++ src/Pages/Appointment/AbailabailAppointment.js
@@ -15,9 +15,9 @@ const AbailabailAppointment = ({ date }) => {
                 <h1 className='text-secondary'>Available Services on {format(date, 'PP')}</h1>
                 <p>Please select a service.</p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
                 {
-                    service.map(servic => <AppointmentServicecCard service={servic}></AppointmentServicecCard>)
+                    service.map(servic => <AppointmentServicecCard key={servic._id} service={servic}></AppointmentServicecCard>)
                 }
             </div>
         </div>
--- src/Pages/Appointment/AppointmentServicecCard.js
+++ src/Pages/Appointment/AppointmentServicecCard.js
@@ -5,10 +5,13 @@ const AppointmentServicecCard = ({ service }) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p> AbaolAbil solot {slots.length}</p>
+                <h2 className="card-title text-secondary">{name}</h2>
+                <p> {slots.length} {slots.length === 0 ? "No Space " : slots.length > 1 ? "Spaces" : "Space"}  Available</p>
+                <p>{
+                    slots.length > 0 ? <span>{slots[0]}</span> : <span className='text-red-500'>Please Try Another Day </span>
+                }</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary" disabled={slots.length === 0}>Buy Now</button>
+                    <button className="btn btn-primary" disabled={slots.length === 0}>Book Appointment</button>
                 </div>
             </div>
         </div>
