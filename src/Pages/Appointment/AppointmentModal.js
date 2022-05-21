549ccb6f1163e65d2ac007a58f88b37cc44f5335
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-05-21 22:22:31 +0600

    appointment modal edite

--- src/Pages/Appointment/AppointmentModal.js
+++ src/Pages/Appointment/AppointmentModal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AppointmentModal = ({ triment }) => {
-    const { name, slots} = triment
+    const { name, slots } = triment
     return (
         <div>
             <input type="checkbox" id="appointment-modal" class="modal-toggle" />
@@ -9,11 +9,19 @@ const AppointmentModal = ({ triment }) => {
                 <div class="modal-box relative">
                     <label for="appointment-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 class="text-lg font-bold text-center text-primary">{name}!</h3>
-                    <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' value={name} type="text" name="Full Name " id="" />
-                    <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' value={slots[0]} type="text" name="Full Name " id="" />
-                    <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' placeholder='Full Name' type="text" name="Full Name " id="" />
-                    <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl' placeholder='Phone Number' type="text" name="Phone Number  " id="" />
-                    <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl' placeholder='Email' type="email" name="Email  " id="" />
+                    <form >
+                        <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' disabled value={name} type="text" name="Full Name " id="" />
+                        <select class="select select-bordered w-full bg-neutral w-full p-2 mt-5 rounded text-white text-xl">
+                            {
+                                slots.map(slot => <option>{slot}</option>)
+                            }
+
+                        </select>
+                        <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' placeholder='Full Name' type="text" name="Full Name " id="" />
+                        <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl' placeholder='Phone Number' type="text" name="Phone Number  " id="" />
+                        <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl' placeholder='Email' type="email" name="Email  " id="" />
+                        <input className='bg-secondary w-full p-2 mt-5 rounded text-white text-xl' type="submit" value="Submit" />
+                    </form>
                 </div>
             </div>
         </div>
