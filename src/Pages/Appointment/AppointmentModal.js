b8dde3e1f71f0c60893c5df78756016e46178056
mdsoliman21 <89044891+mdsoliman-me@users.noreply.github.com>
2022-05-24 01:22:57 +0600

    sumthing add

--- src/Pages/Appointment/AppointmentModal.js
+++ src/Pages/Appointment/AppointmentModal.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 const AppointmentModal = ({ triment, date }) => {
     const { name, slots } = triment;
     const [user, loading, error] = useAuthState(auth)
-    
 
+    if (loading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
             <input type="checkbox" id="appointment-modal" className="modal-toggle" />
@@ -24,7 +27,7 @@ const AppointmentModal = ({ triment, date }) => {
 
                         </select>
                         <input className='bg-neutral w-full p-2 mt-5 rounded text-white text-xl' placeholder='Full Name' disabled value={user.displayName} type="text" name="Full Name " id="" />
-                        <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl'  placeholder='Phone Number' type="text" name="Phone Number  " id="" />
+                        <input className='bg-accent w-full p-2 mt-5 rounded text-white text-xl' placeholder='Phone Number' type="text" name="Phone Number  " id="" />
                         <input className='bg-accent w-full p-2 mt-5 rounded  text-white text-xl' disabled placeholder='Email' value={user.email} type="email" name="Email  " id="" />
                         <input className='bg-secondary w-full p-2 mt-5 rounded text-white text-xl' type="submit" value="Submit" />
                     </form>
--- src/Pages/Appointment/AppointmentServicecCard.js
+++ src/Pages/Appointment/AppointmentServicecCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AppointmentServicecCard = ({ service, setTritment , date}) => {
     const { name, slots } = service;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card w-96 bg-base-100 items-center shadow-xl">
             <div className="card-body">
                 <h2 className="card-title text-secondary">{name}</h2>
                 <p> {slots.length} {slots.length === 0 ? "No Space " : slots.length > 1 ? "Spaces" : "Space"}  Available</p>
@@ -12,7 +12,7 @@ const AppointmentServicecCard = ({ service, setTritment , date}) => {
                 }</p>
                 <div className="card-actions justify-center">
 
-                    <label for="appointment-modal" class="btn btn-primary modal-button" disabled={slots.length === 0} onClick={() => setTritment(service)}>Book Appointment</label>
+                    <label for="appointment-modal" class="btn btn-primary btn-sm mt-2 modal-button" disabled={slots.length === 0} onClick={() => setTritment(service)}>Book Appointment</label>
                 </div>
             </div>
         </div>
